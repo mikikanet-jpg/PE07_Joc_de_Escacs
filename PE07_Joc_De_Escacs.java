e8da270322d7723af41cdc2fa1959900391ac7a3
Miguel Sanchez Cuadros <Mikikanet@gmail.com>
2026-01-15 16:27:51 +0100

    Creacio del fitxer

---
+++ PE07_Joc_De_Escacs.java
@@ -0,0 +1,11 @@
+package Practica7;
+
+public class PE07_Joc_De_Escacs {
+    public static void main(String[] args) {
+    PE07_Joc_De_Escacs p = new PE07_Joc_De_Escacs();
+    p.principal();
+}
+public void principal() {
+    
+}
+}
