81e7bc936a4621195d2ef592270e8ae9fe7238ca
Miguel Sanchez Cuadros <Mikikanet@gmail.com>
2026-01-15 16:41:17 +0100

    Creacio de demanar jugadors i metode per inicialitzarTauler

--- PE07_Joc_De_Escacs.java
+++ PE07_Joc_De_Escacs.java
@@ -1,11 +1,40 @@
 package Practica7;
 
+import java.util.Scanner;
+
 public class PE07_Joc_De_Escacs {
+    char[][] tauler = new char [8][8];
+    Scanner j = new Scanner(System.in);
+    String jugadorBlanc;
+    String jugadorNegre;
     public static void main(String[] args) {
     PE07_Joc_De_Escacs p = new PE07_Joc_De_Escacs();
-    p.principal();
+    p.iniciarJoc();
 }
-public void principal() {
-    
+public void iniciarJoc() {
+    demanarJugadors();
+    inicialitzarTauler();
+
+}
+
+public void demanarJugadors() {
+    do{
+        System.out.println("Jugador Blanques: ");
+        jugadorBlanc = j.nextLine();
+    } while (jugadorBlanc.isEmpty());
+
+    do {
+        System.out.println("Jugador Negres: ");
+        jugadorNegre = j.nextLine();
+    } while (jugadorNegre.isEmpty());
+}
+
+public void inicialitzarTauler() {
+    crearMatriuTauler();
+
+}
+
+public void crearMatriuTauler() {
+    tauler = new char[8][8];
 }
 }
